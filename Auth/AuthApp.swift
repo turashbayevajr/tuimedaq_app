be847cedb3abefbf852004f3b89f4edb813117de
turashbayevajr <103361172+turashbayevajr@users.noreply.github.com>
2023-01-18 00:35:40 +0600

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by Ажар Турашбаева on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
